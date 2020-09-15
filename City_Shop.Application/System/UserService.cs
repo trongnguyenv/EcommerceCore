12b24a4b5fe8e18f246e4acc60f8727d0e0e66d1
trongnguyenv <trongdk586@gmail.com>
2020-09-15 15:02:19 +0700

    login api

---
+++ City_Shop.Application/System/UserService.cs
@@ -0,0 +1,78 @@
+﻿using City_Shop.Data.Entities;
+using City_Shop.ViewModel.System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City_Shop.Application.System
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IConfiguration _config;
+
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _config = config;
+        }
+
+        public async Task<string> Authenticate(LoginRequest request)
+        {
+            //Check Username
+            var user = await _userManager.FindByNameAsync(request.Username);
+            if (user == null) return null;
+            // Check Password
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.Rememberme, true);
+
+            if (!result.Succeeded) { return null; }
+
+            var roles = _userManager.GetRolesAsync(user);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Role, string.Join(";", roles))
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Tokens:Issuer"],
+                _config["Tokens:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new AppUser()
+            {
+                Dob = request.Dob,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.Username,
+                Email = request.Email,
+                PhoneNumber = request.Phone,
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (result.Succeeded) { 
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ City_Shop.ViewModel/System/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace City_Shop.ViewModel.System
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Rememberme { get; set; }
+    }
+}
