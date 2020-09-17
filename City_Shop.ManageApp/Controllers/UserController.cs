0f51270653e6bd998b6b0b43ef0b84170552e33a
trongnguyenv <trongdk586@gmail.com>
2020-09-17 14:24:43 +0700

    add cookie authentication without identity

--- City_Shop.ManageApp/Controllers/UserController.cs
+++ City_Shop.ManageApp/Controllers/UserController.cs
@@ -1,29 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using City_Shop.ManageApp.Services;
+﻿using City_Shop.ManageApp.Services;
 using City_Shop.ViewModel.System.Users;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace City_Shop.ManageApp.Controllers
 {
     public class UserController : Controller
     {
         private readonly IUserApiClient _userApiClient;
+        private readonly IConfiguration _configuration;
 
-        public UserController(IUserApiClient userApiClient)
+        public UserController(IUserApiClient userApiClient, IConfiguration configuration)
         {
             _userApiClient = userApiClient;
+            _configuration = configuration;
         }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public IActionResult Login()
+        public async Task<IActionResult> Login()
         {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return View();
         }
 
@@ -34,8 +44,46 @@ namespace City_Shop.ManageApp.Controllers
                 return View(ModelState);
 
             var token = await _userApiClient.Authenticate(request);
+            // Call token convert user
+            var userPrincipal = ValidateToken(token);
+            // Refreshing the authentication session should be allowed.
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                IsPersistent = false
+            };
 
-            return View(token);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults
+                .AuthenticationScheme, userPrincipal, authProperties);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "User");
+        }
+
+        private ClaimsPrincipal ValidateToken(string jwtToken)
+        {
+            // Microsoft.IdentityModel.Logging
+            IdentityModelEventSource.ShowPII = true;
+            // Microsoft.IdentityModel.Tokens
+            SecurityToken validatedToken;
+
+            TokenValidationParameters validationParameters = new TokenValidationParameters();
+            validationParameters.ValidateLifetime = true;
+            validationParameters.ValidAudience = _configuration["Tokens:Issuer"];
+            validationParameters.ValidIssuer = _configuration["Tokens:Issuer"];
+            validationParameters.IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+
+            // System.IdentityModel.Tokens.Jwt
+            ClaimsPrincipal principal = new JwtSecurityTokenHandler()
+                .ValidateToken(jwtToken, validationParameters, out validatedToken);
+            return principal;
         }
     }
 }
